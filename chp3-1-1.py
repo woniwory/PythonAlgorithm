3dbb6bda2ec166bb935f8129d9efcda3f48d4f95
woniwory <37476534+woniwory@users.noreply.github.com>
2021-12-28 22:33:15 +0900

    Initial Commit

--- chp3-1-1.py
+++ chp3-1-1.py
@@ -1,12 +1,1 @@
-n, m, k = map(int, input().split())
-data = list(map(int, input().split()))
-res = 0
-
-data = sorted(data, reverse = True)
-res =  data[0] * k * (m // k) + data[1] * (m % k)
-
-print(res)
-
-
-#sort와  sorted의 차이
-#얕은 복사와 깊은 복사
+n, m, k = map(int, inp)
