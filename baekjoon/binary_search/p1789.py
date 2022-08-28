a79742f22f25f202d0c9e8ec13fb1f66f3be135a
정성원 <37476534+woniwory@users.noreply.github.com>
2022-08-29 03:33:51 +0900

    수들의 합
    서로 다른 n 개의 자연수의 합을 s라 할 때 자연수 n의 최댓값은?

---
+++ baekjoon/binary_search/p1789.py
@@ -0,0 +1,19 @@
+# 수들의 합 : 서로 다른 n 개의 자연수의 합을 s라 할 때 자연수 n의 최댓값은?
+
+
+s = int(input())
+
+start = 0
+end = s
+
+while start <= end:
+    mid = (start + end // 2)
+    
+    if mid * (mid + 1) // 2 <= s:
+        start = mid + 1
+        
+    else:
+        end = mid - 1
+        
+        
+print(mid)        
