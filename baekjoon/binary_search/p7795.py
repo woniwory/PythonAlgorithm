e833f08376d49416d0c8d35d85e40640da63ff9d
정성원 <37476534+woniwory@users.noreply.github.com>
2022-09-09 12:25:18 +0900

    먹을 것인가 먹힐 것인가
    7795번. 심해에는 두 종류의 생명체 A와 B가 존재한다. A는 B를 먹는다. A는 자기보다 크기가 작은 먹이만 먹을 수 있다. 예를 들어, A의 크기가 {8, 1, 7, 3, 1}이고, B의 크기가 {3, 6, 1}인 경우에 A가 B를 먹을 수 있는 쌍의 개수는 7가지가 있다. 8-3, 8-6, 8-1, 7-3, 7-6, 7-1, 3-1.
    
    두 생명체 A와 B의 크기가 주어졌을 때, A의 크기가 B보다 큰 쌍이 몇 개나 있는지 구하는 프로그램을 작성하시오.

---
+++ baekjoon/binary_search/p7795.py
@@ -0,0 +1,24 @@
+t = int(input())
+answer = [0] * t
+
+for testcase in range(t):
+    
+    n, m = map(int, input().split())
+    nlist = sorted(list(map(int, input().split()))) # 1 1 3 7 8
+    mlist = sorted(list(map(int, input().split()))) # 1 3 6
+    start = 0 
+    end = n
+    while start <= end:
+        mid = (start + end) // 2
+        for i in nlist:
+            for j in mlist:
+                if i > j:
+                    start = mid + 1
+                    answer[testcase] += 1
+                else:
+                    end = mid - 1
+    
+    
+for testcase in range(t):
+    print(answer[testcase])
+
