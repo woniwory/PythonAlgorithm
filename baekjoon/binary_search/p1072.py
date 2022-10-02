55193accabdbe4c5b2acca500a550b483ce17cae
정성원 <37476534+woniwory@users.noreply.github.com>
2022-10-02 15:19:56 +0900

    Create p1072.py

---
+++ baekjoon/binary_search/p1072.py
@@ -0,0 +1,31 @@
+x, y = map(int, input().split())
+z = y // x
+cnt = 0
+
+# while y <= x:
+#     x, y = x+cnt, y+cnt
+#     tmp = y // x
+#     if z != tmp:
+#         print(cnt)
+#         break
+#     else:
+#         cnt += 1
+#         print("cnt++")
+        
+        
+while :
+    cnt += 1
+    if x == y:
+        print(-1)
+        break
+
+    if z != (x+1)//y+1:
+        
+        
+    else:
+        cnt = -1
+        print("cnt++")
+        
+                
+#ㅅㅂ뭐함?
+    
