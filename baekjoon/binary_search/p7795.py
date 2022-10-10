21eb8da6487566ff574bb9fe60206da4d722b950
정성원 <37476534+woniwory@users.noreply.github.com>
2022-10-10 11:45:36 +0900

    Update p7795.py

--- baekjoon/binary_search/p7795.py
+++ baekjoon/binary_search/p7795.py
@@ -1,23 +1,25 @@
+import sys
+
 t = int(input())
 answer = [0] * t
 
 for testcase in range(t):
     
-    n, m = map(int, input().split())
-    nlist = sorted(list(map(int, input().split()))) # 1 1 3 7 8
-    mlist = sorted(list(map(int, input().split()))) # 1 3 6
+    n, m = map(int, sys.stdin.readline().split())
+    nlist = sorted(list(map(int, sys.stdin.readline().split()))) # 1 1 3 7 8
+    mlist = list(map(int, sys.stdin.readline().split())) # 1 3 6
+    
     start = 0 
-    end = n
+    end = n-1
     while start <= end:
         mid = (start + end) // 2
-        for i in nlist:
-            for j in mlist:
-                if i > j:
-                    start = mid + 1
-                    answer[testcase] += 1
-                else:
-                    end = mid - 1
-    
+        for i in mlist:
+            if nlist[mid] > i:
+                start = mid + 1
+                answer[testcase] += 1
+            else:
+                end = mid - 1
+
     
 for testcase in range(t):
     print(answer[testcase])
